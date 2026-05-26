514d80b7b5a117220c20a296473939e4cf1fbfdd
Rafael Farias Castro <rafaelfcastro.eng@gmail.com>
2026-05-26 08:23:44 -0300

    incluindo classe position

--- basic.py
+++ basic.py
@@ -3,17 +3,40 @@ DIGITS = '0123456789'
 
 # errors
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self,pos_start,pos_end ,error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
         
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
+        result += f'Dile {self.pos_start.fn}, line{self.pos_start.ln + 1}'
         return result
     
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    def __init__(self,pos_start,pos_end, details):
+        super().__init__(pos_start,pos_end,'Illegal Character', details)
+
+class Position:
+    def __init__(self,idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col 
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advanced(self,current_char):
+        self.idx +=1
+        self.col +=1
+
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)   
+
+
+
 
 # tokens
 TT_INT = 'INT'
@@ -38,14 +61,15 @@ class Token:
     
     
 class Lexer:
-    def __init__(self, text):
+    def __init__(self,fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1,0,-1,fn,text)
         self.current_char = None
         self.advance()
     
     def advance(self): 
-        self.pos += 1
+        self.pos.advanced(self.current_char)
         self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
         
     def generate_tokens(self):
@@ -69,6 +93,7 @@ class Lexer:
                 tokens.append(Token(TT_DIV))
                 self.advance()
             else:
+                pos_Start = self.pos.copy()
                 char = self.current_char
                 self.advance()
                 return [], IllegalCharError(f"'{char}'")
@@ -94,8 +119,8 @@ class Lexer:
         
 # run
 
-def run(text):
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.generate_tokens()
     
     return tokens, error
